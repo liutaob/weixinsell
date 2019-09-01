57de5319028b85c694f3ee6f878a764e9a07e32c
liut <244160321@qq.com>
2019-09-01 13:07:44 +0800

    add log-test and its config

---
+++ sell/src/test/java/com/jxust/sell/LoggerTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Guangdong Etone Technology Co.,Ltd.
+ * All rights reserved.
+ */
+package com.jxust.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 一句简洁的说明
+ *
+ * @author <a href="mailto:maxid@qq.com">LiuTao</a>
+ * @since $$Id$$
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+
+    @Test
+    public void test1() {
+        log.debug("debug..");
+        log.info("info..");
+        log.error("error..");
+        String name = "liut";
+        String password = "123456";
+        log.info("name: " + name + ",password:" + password);
+        log.info("name: {}, password: {}", name, password);
+        log.warn("warn..");
+    }
+}
